12b2a4803327be28dc611fcfda6b5b68d39856ac
Pedro Nunes <pedronunes.dev@gmail.com>
2023-12-22 19:05:10 -0300

    feat: created template of grid coffee list

--- src/pages/Home/components/CoffeeList/CoffeeList.tsx
+++ src/pages/Home/components/CoffeeList/CoffeeList.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
 import * as Styled from './styles';
+import CardContainer from '../../../../components/CardContainer/CardContainer';
 
 export function CoffeeList() {
   return (
     <Styled.ListContainer>
-      <h2>Nosso cafés</h2>
+      <h2>Our Coffees</h2>
 
-      
+      <Styled.CofeeList>
+        <CardContainer>
+          <Styled.CofeeContent>
+            <h3>Cofee Name</h3>
+          </Styled.CofeeContent>
+        </CardContainer>
+      </Styled.CofeeList>
     </Styled.ListContainer>
   )
 }
