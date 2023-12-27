3c9e47313dabb0e419e56ede8ca9ce6964ab097b
Pedro Nunes <pedronunes.dev@gmail.com>
2023-12-27 19:01:58 -0300

    feat: added coffee mocks

--- src/mocks/coffeeMocks.ts
+++ src/mocks/coffeeMocks.ts
@@ -1,13 +1,110 @@
-type CoffeeTypes = 'REGULAR' | 'ICED' | 'LATTE' | 'SPECIALTY' | 'ALCOHOLIC';
+type CoffeeTypes = 'REGULAR' | 'ICED' | 'WITH_MILK' | 'SPECIALTY' | 'ALCOHOLIC';
 
 interface Coffee {
   image: string,
-  type: CoffeeTypes,
+  type: CoffeeTypes[],
   title: string,
   description: string,
   price: number,
 }
 
 export const coffeeList: Coffee[] = [
-
+  {
+    image: '',
+    type: ['REGULAR'],
+    title: 'Traditional Espresso',
+    description: 'The traditional coffee made with hot water and ground beans',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['REGULAR'],
+    title: 'American Espresso',
+    description: 'Diluted espresso, less intense than the traditional ones',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['REGULAR'],
+    title: 'Creamy Espresso',
+    description: 'Traditional espresso with creamy foam',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['REGULAR', 'ICED'],
+    title: 'Iced Espresso',
+    description: 'Beverage prepared with espresso and ice cubes',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['REGULAR', 'WITH_MILK'],
+    title: 'Coffee with Milk',
+    description: 'Half - and - half of traditional espresso with steamed milk',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['REGULAR', 'WITH_MILK'],
+    title: 'Latte',
+    description: 'A shot of espresso with double the amount of milk and creamy foam',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['REGULAR', 'WITH_MILK'],
+    title: 'Cappuccino',
+    description: 'Beverage with cinnamon made from equal parts of coffee, milk, and foam',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['REGULAR', 'WITH_MILK'],
+    title: 'Macchiato',
+    description: 'Espresso mixed with a little hot milk and foam',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['REGULAR', 'WITH_MILK'],
+    title: 'Mocaccino',
+    description: 'Espresso with chocolate syrup, little milk, and foam',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['SPECIALTY', 'WITH_MILK'],
+    title: 'Hot Chocolate',
+    description: 'Beverage made with chocolate dissolved in hot milk and coffee',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['SPECIALTY', 'ALCOHOLIC', 'ICED'],
+    title: 'Cuban Coffee',
+    description: 'Cold drink made from espresso, rum, cream, and mint',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['SPECIALTY'],
+    title: 'Hawaiian Coffee',
+    description: 'Sweetened drink prepared with coffee and coconut milk',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['SPECIALTY'],
+    title: 'Arabic Coffee',
+    description: 'Beverage prepared with Arabian coffee beans and spices',
+    price: 9.90,
+  },
+  {
+    image: '',
+    type: ['SPECIALTY', 'ALCOHOLIC'],
+    title: 'Irish Coffee',
+    description: 'Coffee - based drink with Irish whiskey, sugar, and whipped cream',
+    price: 9.90,
+  },
 ];
