0eaafdb086acd16a69e7f40efe478cf260ed3c54
Pedro Nunes <pedronunes.dev@gmail.com>
2023-12-28 18:55:56 -0300

    chore: added images to coffee list mock

---
+++ src/assets/index.ts
@@ -0,0 +1,31 @@
+import americanEspresso from './american-espresso.png';
+import arabic from './arabic.png';
+import cappuccino from './cappuccino.png';
+import coffeWithMilk from './coffe-with-milk.png';
+import creamyEspresso from './creamy-espresso.png';
+import cuban from './cuban.png';
+import espresso from './espresso.png';
+import hawaiian from './hawaiian.png';
+import hotChocolate from './hot-chocolate.png';
+import icedEspresso from './iced-espresso.png';
+import irish from './irish.png';
+import latte from './latte.png';
+import macchiato from './macchiato.png';
+import mocaccino from './mocaccino.png';
+
+export {
+  americanEspresso,
+  arabic,
+  cappuccino,
+  coffeWithMilk,
+  creamyEspresso,
+  cuban,
+  espresso,
+  hawaiian,
+  hotChocolate,
+  icedEspresso,
+  irish,
+  latte,
+  macchiato,
+  mocaccino,
+}
--- src/mocks/coffeeMocks.ts
+++ src/mocks/coffeeMocks.ts
@@ -1,3 +1,5 @@
+import * as coffeImage from '../assets'
+
 type CoffeeTypes = 'REGULAR' | 'ICED' | 'WITH_MILK' | 'SPECIALTY' | 'ALCOHOLIC';
 
 interface Coffee {
@@ -10,98 +12,98 @@ interface Coffee {
 
 export const coffeeList: Coffee[] = [
   {
-    image: '',
+    image: coffeImage.americanEspresso,
     type: ['REGULAR'],
     title: 'Traditional Espresso',
     description: 'The traditional coffee made with hot water and ground beans',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.americanEspresso,
     type: ['REGULAR'],
     title: 'American Espresso',
     description: 'Diluted espresso, less intense than the traditional ones',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.creamyEspresso,
     type: ['REGULAR'],
     title: 'Creamy Espresso',
     description: 'Traditional espresso with creamy foam',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.icedEspresso,
     type: ['REGULAR', 'ICED'],
     title: 'Iced Espresso',
     description: 'Beverage prepared with espresso and ice cubes',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.coffeWithMilk,
     type: ['REGULAR', 'WITH_MILK'],
     title: 'Coffee with Milk',
     description: 'Half - and - half of traditional espresso with steamed milk',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.latte,
     type: ['REGULAR', 'WITH_MILK'],
     title: 'Latte',
     description: 'A shot of espresso with double the amount of milk and creamy foam',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.cappuccino,
     type: ['REGULAR', 'WITH_MILK'],
     title: 'Cappuccino',
     description: 'Beverage with cinnamon made from equal parts of coffee, milk, and foam',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.macchiato,
     type: ['REGULAR', 'WITH_MILK'],
     title: 'Macchiato',
     description: 'Espresso mixed with a little hot milk and foam',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.mocaccino,
     type: ['REGULAR', 'WITH_MILK'],
     title: 'Mocaccino',
     description: 'Espresso with chocolate syrup, little milk, and foam',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.hotChocolate,
     type: ['SPECIALTY', 'WITH_MILK'],
     title: 'Hot Chocolate',
     description: 'Beverage made with chocolate dissolved in hot milk and coffee',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.cuban,
     type: ['SPECIALTY', 'ALCOHOLIC', 'ICED'],
     title: 'Cuban Coffee',
     description: 'Cold drink made from espresso, rum, cream, and mint',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.hawaiian,
     type: ['SPECIALTY'],
     title: 'Hawaiian Coffee',
     description: 'Sweetened drink prepared with coffee and coconut milk',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.arabic,
     type: ['SPECIALTY'],
     title: 'Arabic Coffee',
     description: 'Beverage prepared with Arabian coffee beans and spices',
     price: 9.90,
   },
   {
-    image: '',
+    image: coffeImage.irish,
     type: ['SPECIALTY', 'ALCOHOLIC'],
     title: 'Irish Coffee',
     description: 'Coffee - based drink with Irish whiskey, sugar, and whipped cream',
