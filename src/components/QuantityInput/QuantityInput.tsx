cedf1d5816a4ed33f68e43841b1f4d7db010e3af
Pedro Nunes <pedronunes.dev@gmail.com>
2024-02-13 07:05:37 -0300

    style: improved quantity input styles

--- src/components/QuantityInput/QuantityInput.tsx
+++ src/components/QuantityInput/QuantityInput.tsx
@@ -3,26 +3,26 @@ import * as Styled from './styles';
 import { useState } from 'react';
 
 export default function QuantityInput() {
-	const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(1);
 
-	const increaseQuantity = () => {
-		setQuantity((currentValue) => currentValue + 1);
-	};
+  const increaseQuantity = () => {
+    setQuantity((currentValue) => currentValue + 1);
+  };
 
-	const decreaseQuantity = () => {
-		if (quantity === 1) return;
-		setQuantity((currentValue) => currentValue - 1);
-	};
+  const decreaseQuantity = () => {
+    if (quantity === 1) return;
+    setQuantity((currentValue) => currentValue - 1);
+  };
 
-	return (
-		<Styled.InputContainer>
-			<button onClick={decreaseQuantity}>
-				<Minus size={14} weight="bold" />
-			</button>
-			<span>{quantity}</span>
-			<button onClick={increaseQuantity}>
-				<Plus size={14} weight="bold" />
-			</button>
-		</Styled.InputContainer>
-	);
+  return (
+    <Styled.InputContainer>
+      <button onClick={decreaseQuantity}>
+        <Minus size={14} weight="bold" />
+      </button>
+      <span>{quantity}</span>
+      <button onClick={increaseQuantity}>
+        <Plus size={14} weight="bold" />
+      </button>
+    </Styled.InputContainer>
+  );
 }
--- src/components/QuantityInput/styles.ts
+++ src/components/QuantityInput/styles.ts
@@ -1,20 +1,25 @@
 import styled from 'styled-components';
 
 export const InputContainer = styled.div`
-	width: 4.5rem;
-	padding: 0.5rem;
-	height: 2.375rem;
-	border-radius: 0.375rem;
-	background-color: ${(props) => props.theme['base-button']};
-	color: ${(props) => props.theme['base-title']};
+  width: 4.5rem;
+  padding: 0.5rem;
+  height: 2.375rem;
+  border-radius: 0.375rem;
+  background-color: ${(props) => props.theme['base-button']};
+  color: ${(props) => props.theme['base-title']};
 
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 
-	button {
-		border: none;
-		background: none;
-		color: ${(props) => props.theme['purple']};
-	}
+  button {
+    border: none;
+    background: none;
+    color: ${(props) => props.theme['purple']};
+    cursor: pointer;
+
+    &:hover {
+      color: ${(props) => props.theme['purple-dark']};
+    }
+  }
 `;
